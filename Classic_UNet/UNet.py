574055a2be968e0f208db40e87eed4c5fb13f27e
Cyril Palerme <cyrilp@met.no>
2024-06-10 15:58:22 +0000

    update Classic_UNet

--- Classic_UNet/UNet.py
+++ Classic_UNet/UNet.py
@@ -13,7 +13,7 @@ import tensorflow as tf
 
 
 class UNet():
-    def __init__(self, list_predictors, list_targets, patch_dim, batch_size, n_filters, activation, kernel_initializer, batch_norm, pooling_type, dropout):
+    def __init__(self, list_predictors, list_targets, patch_dim, batch_size, n_filters, activation, kernel_initializer, batch_norm, pooling_type):
         self.list_predictors = list_predictors
         self.list_targets = list_targets
         self.patch_dim = patch_dim
@@ -23,7 +23,6 @@ class UNet():
         self.kernel_initializer = kernel_initializer
         self.batch_norm = batch_norm
         self.pooling_type = pooling_type
-        self.dropout = dropout
         self.n_predictors = len(list_predictors)
     #
     def conv_block(self, x, n_filters, padding = "same"):
@@ -47,13 +46,11 @@ class UNet():
         elif self.pooling_type == "Average":
             p = tf.keras.layers.AveragePooling2D(pool_size = pool_size, strides = strides)(f)
         #
-        p = tf.keras.layers.Dropout(self.dropout)(p)
         return(f, p)  
     #
     def upsample_block(self, x, conv_features, n_filters, kernel_size = (2,2), strides = 2, padding = "same"):
         x = tf.keras.layers.Conv2DTranspose(n_filters, kernel_size = kernel_size, strides = strides, padding = padding)(x)
         x = tf.keras.layers.concatenate([x, conv_features])
-        x = tf.keras.layers.Dropout(self.dropout)(x)
         x = self.conv_block(x, n_filters)
         return(x)
     #
@@ -74,8 +71,8 @@ class UNet():
         u1 = self.upsample_block(u2, f2, self.n_filters[1])
         u0 = self.upsample_block(u1, f1, self.n_filters[0])
         # outputs
-        SIC = tf.keras.layers.Conv2D(1, (1, 1), padding = "same", activation = "linear", dtype = tf.float32, name = "SIC")(u0)
-        unet_model = tf.keras.Model(inputs, SIC, name = "U-Net")
+        output = tf.keras.layers.Conv2D(1, (1, 1), padding = "same", activation = "linear", dtype = tf.float32, name = "SIC")(u0)
+        unet_model = tf.keras.Model(inputs, output, name = "U-Net")
         #
         return(unet_model)
 
